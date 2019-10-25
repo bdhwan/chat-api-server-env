8f3a23c6bb077d89579d3c34b7109462da736344
bdhwan <bdhwan@gmail.com>
2019-10-25 10:02:57 +0900

    fix

--- check.sh
+++ check.sh
@@ -4,7 +4,17 @@ echo $GIT_URL
 
 #restart cron
 service cron restart
-# git clone $GIT_URL
+
 git clone "$GIT_URL" && cd "$(basename "$GIT_URL" .git)"
+
+if [ -z "$PM2_INSTANCE" ]
+then
+    echo "no instance count set"
+else 
+    echo "have instance count set-"$PM2_INSTANCE
+    sed "s/instances: 1/instances: "$PM2_INSTANCE"/g" process.yml
+fi
+
+
 npm install --production
 pm2-docker process.yml
--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-docker build  --no-cache --tag bdhwan/chat-api-server-env:0.0.1 .
-docker push bdhwan/chat-api-server-env:0.0.1
+docker build  --no-cache --tag bdhwan/chat-api-server-env:0.0.2 .
+docker push bdhwan/chat-api-server-env:0.0.2
